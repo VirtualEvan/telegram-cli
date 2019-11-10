96117141057b9b3510f2563effc7a39e15268a44
Esteban Puentes <virtualevan@gmail.com>
2019-11-10 23:58:35 +0100

    Manage bot settings

---
+++ config.py
@@ -0,0 +1,34 @@
+###################
+# Config settings #
+###################
+
+import configparser
+import sys
+
+# Load settings
+Config = configparser.ConfigParser()
+settings_file = "settings.ini"
+
+if not Config.read(settings_file):
+    print("ERROR: Could not find 'settings.ini'")
+    sys.exit(1)
+
+if not Config.get("bot", "token"):
+    print("ERROR: Token configuration is empty")
+    sys.exit(1)
+
+if not Config.get("directory", "logs_directory"):
+    print("ERROR: Logs configuration is empty")
+    sys.exit(1)
+
+
+def get_token():
+    return Config.get("bot", "token")
+
+
+def get_logs_dir():
+    return Config.get("directory", "logs_directory")
+
+
+def get_chat_id():
+    return Config.get("master", "chat_id") or False
