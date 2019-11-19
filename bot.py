9df6b90ce0f10530365aa7a9fff0304be81980a9
Esteban Puentes <virtualevan@gmail.com>
2019-11-20 00:05:55 +0100

    Base functionalities

---
+++ bot.py
@@ -0,0 +1,34 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+import config
+import os
+
+updater = Updater(token=config.get_token(), use_context=True)
+
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="The Lord of Cinder welcomes you!")
+
+
+def chat_id(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.chat_id if update.message.chat_id == int(config.get_chat_id()) else "nanai")
+
+
+def ls(update, context):
+    stream = os.popen('dir')
+    output = stream.read()
+    context.bot.send_message(chat_id=update.message.chat_id, text="```\n"+output+"\n```", parse_mode="markdown")
+
+
+start_handler = CommandHandler('start', start)
+chat_id_handler = CommandHandler('chatid', chat_id)
+ls_handler = CommandHandler('ls', ls)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(chat_id_handler)
+dispatcher.add_handler(ls_handler)
+
+updater.start_polling()
+updater.idle()
