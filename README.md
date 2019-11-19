958bfa37b42eb5e2fc4024ef98b5d6f1d9ba888e
Esteban Puentes <virtualevan@gmail.com>
2019-11-20 00:53:30 +0100

    Add chatid-based security

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # telegram-cli
+
+https://docs.microsoft.com/en-us/azure/devops/repos/git/ignore-files?view=azure-devops&tabs=visual-studio#ignore-changes-to-committed-files
--- bot.py
+++ bot.py
@@ -9,18 +9,33 @@ dispatcher = updater.dispatcher
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="The Lord of Cinder welcomes you!")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="The Lord of Cinder welcomes you!")
 
 
 def chat_id(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.chat_id if update.message.chat_id == int(config.get_chat_id()) else "nanai")
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=update.message.chat_id)
 
 
 def ls(update, context):
     stream = os.popen('dir')
     output = stream.read()
-    context.bot.send_message(chat_id=update.message.chat_id, text="```\n"+output+"\n```", parse_mode="markdown")
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=f"```{output}```",
+                             parse_mode="markdown")
 
+
+# Security response
+def block(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=f"You have no power here! `{update.message.chat_id}`",
+                             parse_mode="markdown")
+
+
+# Security handler
+block_handler = MessageHandler(~ Filters.chat(int(config.get_chat_id())), block)
+dispatcher.add_handler(block_handler)
 
 start_handler = CommandHandler('start', start)
 chat_id_handler = CommandHandler('chatid', chat_id)
